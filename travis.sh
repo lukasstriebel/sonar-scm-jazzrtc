54e025550d0672a464c1fd0bffc8a5fe9d1147be
Tom <thomas.verin@sonarsource.com>
2016-04-11 14:37:17 +0200

    Setup QA

---
+++ travis.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function installTravisTools {
+  mkdir ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v27 | tar zx --strip-components 1 -C ~/.local
+  source ~/.local/bin/install
+}
+
+installTravisTools
+
+case "$TARGET" in
+
+CI)
+  #regular_mvn_build_deploy_analyze
+
+  SONAR_PROJECT_VERSION=`maven_expression "project.version"`
+ 
+  # Do not deploy a SNAPSHOT version but the release version related to this build
+  set_maven_build_version $TRAVIS_BUILD_NUMBER
+ 
+  # the profile "deploy-sonarsource" is defined in parent pom v28+
+  mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar \
+    -Pcoverage-per-test,deploy-sonarsource \
+    -Dmaven.test.redirectTestOutputToFile=false \
+    -Dsonar.host.url=$SONAR_HOST_URL \
+    -Dsonar.login=$SONAR_TOKEN \
+    -Dsonar.projectVersion=$SONAR_PROJECT_VERSION \
+    -B -e -V
+  ;;
+
+
+*)
+  echo "Unexpected TARGET value: $TARGET"
+  exit 1
+  ;;
+
+esac
